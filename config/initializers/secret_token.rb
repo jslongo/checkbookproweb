d58814aee06469db5347db53b63dd04c6da0fc3e
John Longo <jlongo@summa-tech.com>
2013-03-14 12:37:55 -0400

    intiial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkbookproweb::Application.config.secret_token = 'c0b1149e863959c55daad2ca3f488e9a8dbffadf632d711291056e8688612ec22a7cd2718a47a7b0d6a5d0316f9f5d395b4f71465c7fd1cf96a8b038f95207e8'
